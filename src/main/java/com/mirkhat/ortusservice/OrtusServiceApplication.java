9c9e1aa5c69412b6d1a9cc6cf1331f178d0239c6
Mirkhat Asen <asen.mirkhat@bk.ru>
2022-02-24 13:54:08 +0600

    Initial commit

---
+++ src/main/java/com/mirkhat/ortusservice/OrtusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mirkhat.ortusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrtusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrtusServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mirkhat/ortusservice/OrtusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirkhat.ortusservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrtusServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
